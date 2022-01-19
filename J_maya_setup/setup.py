5d9b9f15ccb100cadfe8f9b715d46b3822414ca1
choujosephj <choujosephj@gmail.com>
2022-01-19 12:17:39 -0700

    deleted extraneous files

--- J_maya_setup/setup.py
+++ J_maya_setup/setup.py
@@ -7,6 +7,13 @@ class J_maya_lib_setup():
         self.name = 'startup'
         self.script_dir = cmds.internalVar(userScriptDir=True)
         self.setup_path = os.path.dirname(__file__)
+        #if file to be moved does exist
+        if os.path.exists('{}/userSetup.py'.format(self.setup_path)):
+            #if userSetup doesn't exist
+            if not os.path.exists('{}/userSetup.py'.format(self.script_dir)):
+                shutil.move('{}/userSetup.py'.format(self.setup_path), '{}/userSetup.py'.format(self.script_dir))
+            else:
+                print('file already used')
         print(self.script_dir)
         print(self.setup_path)
 
